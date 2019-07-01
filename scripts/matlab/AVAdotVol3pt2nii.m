9153c0a1892da6ab2fe035874a5257beafe67f84
Magnotta <magnottav@dhcp80ffdf60.dynamic.uiowa.edu>
2019-07-01 10:26:13 -0500

    UPD: Added scripts from Iowa to handle integeration of with AtlasViwer

---
+++ scripts/matlab/AVAdotVol3pt2nii.m
@@ -0,0 +1,105 @@
+function AVAdotVol3pt2nii(subjectDir, nirsFileName)
+% AVAdotVol3pt2nii(subjectDir) Converts
+%       an AtlasViewer AdotVol.3pt which contains the
+%       data from the individual channels to a NIFTI
+%       image for each channel.
+%
+
+
+% Set the standard filenames used by AtlasViwer
+profileFileName=strcat(subjectDir,'/fw/AdotVol.3pt');
+fwVoxFileName=strcat(subjectDir,'/fw/headvol.vox');
+anatVoxFileName=strcat(subjectDir,'/anatomical/headvol.vox');
+
+% Basename for the output data
+AdotNiftFileBase=strcat(subjectDir,'/viewer/Subject/AdotVol');
+
+% Read the Forward Model Headvol file
+fwVox=load_vox( fwVoxFileName );
+tissueMask = (fwVox.img >= 3);
+dims=size(fwVox.img);
+nx=dims(1);
+ny=dims(2);
+nz=dims(3);
+
+% Now determine the direction of the shift based on the image orientation
+if (fwVox.T_2ras(1,1) < 0.0)
+  signX = -1.0;
+else
+  signX = 1.0;
+end
+if (fwVox.T_2ras(2,2) < 0.0)
+  signY = -1.0;
+else
+  signY = 1.0;
+end
+if (fwVox.T_2ras(3,3) < 0.0)
+  signZ = -1.0;
+else
+  signZ = 1.0;
+end
+
+% Read the Anatomical Vox file using the tools provided in AtlasViewer
+anatVox=load_vox( anatVoxFileName );
+
+% Read the nirs file to get the number of channels
+isOctave = exist('OCTAVE_VERSION') ~= 0;
+if (isOctave)
+  load(nirsFileName);
+else
+  load(nirsFileName,'-mat');
+end
+k = find(SD.MeasList(:,4)==1);
+probe = SD.MeasList(k,:);
+nMeas = size(probe,1);
+
+
+% Read the 3pt file - convert to a 3D array - Write as Nifti
+fid = fopen(profileFileName,'r');
+for i=[0:nMeas-1]
+  fseek(fid, nx*ny*nz*4*i, 'bof');
+  tmpImage=fread(fid,nx*ny*nz,'single');
+  vox=reshape(tmpImage, dims);
+  vox=vox .* tissueMask;
+
+  T_2mcInv =  inv(fwVox.T_2digpts);
+  resampleImage = xform_apply_vol_smooth(vox, T_2mcInv);
+
+  % Create the NIFTI Image
+  spacing=[abs(fwVox.T_2ras(1,1)),abs(fwVox.T_2ras(2,2)),abs(fwVox.T_2ras(3,3))];
+  origin=size(fwVox.img)/2;
+  datatype=16;
+  description='3ptToNii';
+  nii = make_nii(resampleImage,spacing,origin,datatype,description);
+
+  % Update the NIFTI header Information. The logic here is outlined in
+  % the AVfwVol2AnatNii function.
+  origDims = size(anatVox.img);
+  newDims  = size(resampleImage);
+  offsetx = (newDims(1) - origDims(1)) / 2.0 * spacing(1) * signX;
+  offsety = (newDims(2) - origDims(2)) / 2.0 * spacing(2) * signY;
+  offsetz = (newDims(3) - origDims(3)) / 2.0 * spacing(3) * signZ;
+
+  nii.hdr.hist.srow_x=[fwVox.T_2ras(1,1),fwVox.T_2ras(1,2),fwVox.T_2ras(1,3),fwVox.T_2ras(1,4)-offsetx];
+  nii.hdr.hist.srow_y=[fwVox.T_2ras(2,1),fwVox.T_2ras(2,2),fwVox.T_2ras(2,3),fwVox.T_2ras(2,4)-offsety];
+  nii.hdr.hist.srow_z=[fwVox.T_2ras(3,1),fwVox.T_2ras(3,2),fwVox.T_2ras(3,3),fwVox.T_2ras(3,4)-offsetz];
+  nii.hdr.hist.sform_code = 1;
+  nii.hdr.hist.qform_code = 0;
+  nii.hdr.hist.quatern_d = 1.0;
+
+  % Set the NIFTI FIlename based on source and detectors
+  sdpair=SD.MeasList(i+1,:);
+  source=sdpair(1);
+  detector=sdpair(2);
+  sourceStr=int2str(source);
+  detectorStr=int2str(detector);
+  channelStr=int2str(i+1);
+  disp(channelStr)
+  AdotNiftFileName=strcat(AdotNiftFileBase,'_S',sourceStr,'_D',detectorStr,'_C',channelStr,'.nii');
+
+  % Write out the NIFTI Image
+  save_nii(nii, AdotNiftFileName);
+
+end;
+
+fclose(fid);
---
+++ scripts/matlab/digPtsToAnatomical.m
@@ -0,0 +1,90 @@
+function digPtsToAnatomical(subjectDir)
+% digPtsToAnatomical(subjectDir) Converts the digpts file
+% to subject space. The resulting points are written out in
+% Slicer Fiducial format.
+%
+
+% Set the standard filenames used by AtlasViwer
+fwVoxFileName=strcat(subjectDir,'/fw/headvol.vox');
+anatVoxFileName=strcat(subjectDir,'/anatomical/headvol.vox');
+anatVoxFileName=strcat(subjectDir,'/anatomical/headvol.vox');
+
+
+% Basename for the output data
+%outputFile=strcat(subjectDir,'/viewer/Subject/digpts.fcsv');
+outputFile=strcat(subjectDir,'/anatomical/digptsTest.fcsv');
+
+digpts = initDigpts();
+digpts = getDigpts(digpts, subjectDir);
+refpts = initRefpts();
+refpts = getRefpts(refpts,subjectDir);
+
+numLandmarks = size(digpts.refpts.labels,2);
+numSource = size(digpts.srcpos,1);
+numDetector = size(digpts.detpos,1);
+
+% Read the Forward Model Headvol file
+fwVox=load_vox( fwVoxFileName );
+
+% Read the Anatomical Vox file using the tools provided in AtlasViewer
+anatVox=load_vox( anatVoxFileName );
+
+
+
+%
+% Create the Header for the Slicer fiducial file
+%
+fileID = fopen(outputFile,'w');
+fprintf(fileID,'# Fiducial List file %s\n', outputFile);
+fprintf(fileID,'# name = digpts\n');
+fprintf(fileID,'# numPoints = %d\n');
+fprintf(fileID,'# symbolScale = 10\n');
+fprintf(fileID,'# symbolType = 12\n');
+fprintf(fileID,'# visibility = 1\n');
+fprintf(fileID,'# textScale = 7.0\n');
+fprintf(fileID,'# color = 0.4,1,1\n');
+fprintf(fileID,'# selectedColor = 0.807843,0.560784,1\n');
+fprintf(fileID,'# opacity = 1\n');
+fprintf(fileID,'# ambient = 0\n');
+fprintf(fileID,'# diffuse = 1\n');
+fprintf(fileID,'# specular = 0\n');
+fprintf(fileID,'# power = 1\n');
+fprintf(fileID,'# locked = 0\n');
+fprintf(fileID,'# columns = label,x,y,z,sel,vis\n');
+
+
+% Convert the location of the reference points to Subject Space
+subjectSpaceRefPts = xform_apply(digpts.refpts.pos, inv(fwVox.T_2digpts));
+subjectSpaceRefPts = xform_apply(subjectSpaceRefPts, fwVox.T_2ras);
+
+for i=[1:numLandmarks]
+  fprintf(fileID,'%s,%f,%f,%f,0,1\n',string(digpts.refpts.labels(i)),subjectSpaceRefPts(i,1),subjectSpaceRefPts(i,2),subjectSpaceRefPts(i,3));
+end;
+
+
+% Read the headsurf file - Source and Detectors are projected to this surface
+headsurf = initHeadsurf();
+headsurf = getHeadsurf(headsurf, subjectDir);
+
+% Convert the location of the Source Optodes
+subjectSpaceSource = xform_apply(digpts.srcpos, inv(fwVox.T_2digpts));
+subjectSpaceSource = pullPtsToSurf(subjectSpaceSource, headsurf, 'center', 0, false);
+subjectSpaceSource = xform_apply(subjectSpaceSource, fwVox.T_2ras);
+
+for i=[1:numSource]
+  fprintf(fileID,'%s,%f,%f,%f,0,1\n','S'+string(i),subjectSpaceSource(i,1),subjectSpaceSource(i,2),subjectSpaceSource(i,3));
+end;
+
+% Convert the location of the Detector Optodes
+subjectSpaceDetect = xform_apply(digpts.detpos, inv(fwVox.T_2digpts));
+subjectSpaceDetect = pullPtsToSurf(subjectSpaceDetect, headsurf, 'center', 0, false);
+subjectSpaceDetect = xform_apply(subjectSpaceDetect, fwVox.T_2ras);
+
+for i=[1:numSource]
+  fprintf(fileID,'%s,%f,%f,%f,0,1\n','D'+string(i),subjectSpaceDetect(i,1),subjectSpaceDetect(i,2),subjectSpaceDetect(i,3));
+end;
+
+fclose(fileID);
+
+
+
