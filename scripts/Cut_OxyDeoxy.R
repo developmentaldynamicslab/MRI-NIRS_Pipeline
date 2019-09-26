3f6e1fe61f9fcd05b002dee564e5cc113c46ca96
Sam <samuel.h.forbes@gmail.com>
2019-09-26 17:23:14 +0100

    Update Cut_OxyDeoxy.R

--- scripts/Cut_OxyDeoxy.R
+++ scripts/Cut_OxyDeoxy.R
@@ -5,17 +5,17 @@ library(dplyr)
 file_list <- list.files('/Volumes/USB DISK/Final_O/', '.csv')
 file_list2 <- as.list(file_list)
 file_list3 <- lapply(file_list2, function(x) paste('/Volumes/USB DISK/Final_O/', x, sep = ''))
-mylist <- lapply(file_list3, read_csv)
-View(mylist[[1]])
+mylist <- lapply(file_list3, function(x) read_csv(x, col_names = F))
+#View(mylist[[1]])
 
 mylist2 <- lapply(mylist, function(x) x[1:3])
 
 names(mylist2) <- file_list2
 
-dir <- '/Volumes/USB DISK/Output/'
+dir <- '/Volumes/USB DISK/Output2/'
 dir.create(dir)
 
 for(i in 1:length(mylist2)){
   a <- paste(dir,file_list2[i], sep = '')
-  write_csv(mylist2[[i]], a)
+  write_csv(mylist2[[i]], a, col_names = F)
 }
