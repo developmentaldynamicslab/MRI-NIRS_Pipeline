98de48700ebc089b66bea1f3859563707cb93417
Sam Forbes <samuel.h.forbes@gmail.com>
2019-10-01 11:57:20 +0100

    Update Cut_OxyDeoxy.R
    * Comments

--- scripts/Cut_OxyDeoxy.R
+++ scripts/Cut_OxyDeoxy.R
@@ -1,9 +1,14 @@
+####################################################################
+#Cut oxy deoxy
+# Use for removing extraneous columns from the Beta output
+###################################################################
 library(readr)
 library(tidyr)
 library(dplyr)
+#To run, check packages are installed, change filepath and outputpath, then run all
 
-filepath = '/Users/nfb15zpu/Documents/J-Files/Grants/Grant_NIH_2013_NIRS/NIHVWM2019/30NIH-VWM-Y1NEW/Final_O'
-outputpath <- '/Users/nfb15zpu/Documents/J-Files/Grants/Grant_NIH_2013_NIRS/NIHVWM2019/30NIH-VWM-Y1NEW/Output2/'
+filepath = '/Users/nfb15zpu/Documents/J-Files/Grants/Grant_NIH_2013_NIRS/NIHVWM2019/30NIH-VWM-Y1NEW/Final_O' #where files are stored
+outputpath <- '/Users/nfb15zpu/Documents/J-Files/Grants/Grant_NIH_2013_NIRS/NIHVWM2019/30NIH-VWM-Y1NEW/Output2/' #output folder
 
 file_list <- list.files(filepath, '.csv')
 file_list2 <- as.list(file_list)
