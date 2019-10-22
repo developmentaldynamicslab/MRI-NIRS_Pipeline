f5ca95667f286928baebb70bdd2c668e73a23821
John Spencer <j.spencer@uea.ac.uk>
2019-10-22 15:36:47 +0100

    updating instructions, adding in sample scripts, cleaning up an output issue

--- scripts/matlab/createfNIRSBetaImages.m
+++ scripts/matlab/createfNIRSBetaImages.m
@@ -58,7 +58,8 @@ for sub=1:numSubjects
     oxyName=strcat(subjectList{4}{sub},'/Final_O_', subjectList{1}{sub},'.csv');
     fileID = fopen(oxyName,'r');
     if fileID < 0
-        'Failed to open the NIRS beta file for reading'
+        disp('Failed to open the NIRS beta file for reading');
+        disp(oxyName);
     else
                 
         oxyData=importdata(strcat(subjectList{4}{sub},'/Final_O_', subjectList{1}{sub},'.csv'));
